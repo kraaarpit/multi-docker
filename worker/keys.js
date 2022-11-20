2f549ae6f8e6a2a54df14e61c49a52c69ee1342b
Arpit Singh <kraa.arpit@gmail.com>
2022-11-20 10:40:48 +0530

    kubernetes setup

--- worker/keys.js
+++ worker/keys.js
@@ -2,3 +2,8 @@ module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
 };
+
+// In Local/Dev Deployemnt,
+// worker Deployemnt can access redis service
+// using redis-cluster-ip
+// using host as http://redis-cluster-ip-service
